d1ee067902d210f4cb126e5cb40ee37e5d1c00a6
kartikmukati <kartikmukati2021@gmail.com>
2020-11-11 23:02:03 +0530

    Final Changes

--- js/index.js
+++ js/index.js
@@ -1,99 +1,148 @@
+//Function to return input values
 const getValues = () => {
-    var billAmount = document.getElementById('bill-amount').value
-    billAmount = billAmount.replace(/\s+/g, '')
-    var tipPercent = parseInt(document.getElementById('tip').value)
-    var people = parseInt(document.getElementById('people').value)
-    return { billAmount: billAmount, tipPercent: tipPercent, people: people }
+    var status = true;
+    var isException = false;
+    try
+    {
+        var billAmount = document.getElementById('bill-amount').value
+        var tipPercent = document.getElementById('tip').value
+        var people = document.getElementById('people').value
+    }
+    catch(error)
+    {
+        isException = true;
+    }
+    billAmount = billAmount.replace(/\s+/g, '');
+    var parsedBillAmount = parseFloat(billAmount)
+    if(isNaN(billAmount) || billAmount.length <= 0 || parsedBillAmount < 0 || isNaN(tipPercent) || isNaN(people))
+        status = false
+    else
+    {
+        var parsedTipPercent = parseFloat(tipPercent)
+        var parsedPeople = parseInt(people)
+    }
+    return { billAmount: parsedBillAmount, tipPercent: parsedTipPercent, people: parsedPeople, status: status, isException: isException }
 }
-
+//Responsible for handling changes in input field
 const onInputChange = () => 
 {
-    var {billAmount, tipPercent, people} = getValues()
+    var {billAmount, tipPercent, people, status, isException} = getValues()
     
-    change(billAmount, tipPercent, people)
+    if(status == true && isException == false) 
+        change(billAmount, tipPercent, people);
+    else if(isException == true)
+        alert('Something went wrong');
+    else if(status == false)
+        alert('Please enter valid number');
 }
-
+//Below function will return type of operation perfomed
+const getOperation = (event) => {
+    var id;
+    try 
+    {
+        id = event.id    
+    } 
+    catch (error) 
+    {
+        console.log('Can not read property of id undefined');
+    }
+    return id;
+}
+//Function to add and subtract the Tip percent, 
 const tipCalculate = (event) => 
 {
-    
-    var {billAmount, tipPercent, people} = getValues()
+    var {billAmount, tipPercent, people, status, isException} = getValues()
 
-    if (isNaN(billAmount) || billAmount.length <= 0) 
-        alert('Enter a valid amount');
-
-    else if(billAmount > 0)
+    if (status == true)
     {
-        var id;
-        try 
-        {
-            id = event.id    
-        } 
-        catch (error) 
-        {
-            console.log('Can not read property of id undefined');
-        }
+        var id = getOperation(event);
+
         var tipPercent;
-        if(id === 'btn-plus-tip') 
+        //If user press '-' button for tip percent
+        if(id === 'btn-plus-tip' && people >= 0) 
         {
             document.getElementById('tip').value = tipPercent + 1;
             tipPercent = parseInt(document.getElementById('tip').value);
-            if (tipPercent > 0) 
-                document.getElementById('btn-minus-tip').disabled = false;
         } 
-        else if(id == 'btn-minus-tip') 
+        //If user press '+' button for tip percent
+        else if(id == 'btn-minus-tip' && people >= 0) 
         {
             document.getElementById('tip').value = tipPercent - 1;
-            tipPercent = parseInt(document.getElementById('tip').value);
-            if (tipPercent <= 0) 
-                document.getElementById('btn-minus-tip').disabled = true;
+            tipPercent = parseInt(document.getElementById('tip').value);                
         }
         change(billAmount, tipPercent, people) ;
     }
-}
+    //If any negative or string containing values passed
+    else if (status == false && isException != true)
+        alert('Please enter valid number');
+    //For handling any exception
+    else if(isException == true)
+        alert('Something went wrong');
 
+}
+//Function to add and subtract the Number of People
 const peopleCalculate = (event) => 
 {
-    var {billAmount, tipPercent, people} = getValues();
+    var {billAmount, tipPercent, people, status, isException} = getValues();
 
-    if (isNaN(billAmount) || billAmount.length <= 0 ) 
-        alert('Enter a valid amount');
-
-    else if(billAmount > 0)
+    if (status == true)
     {
-        var id;
-        try 
-        {
-            id = event.id;
-        } 
-        catch (error) 
-        {
-            console.log('Can not read property of id undefined');
-        }
-        if(id == 'btn-plus-people') 
+        var id = getOperation(event);
+        console.log(id)
+
+        if(id == 'btn-plus-people' && tipPercent >= 0) 
         {
             document.getElementById('people').value = people + 1;
             people = parseInt(document.getElementById('people').value);
-            if (people > 0) 
-                document.getElementById('btn-minus-people').disabled = false;
         }
-        else if(id == 'btn-minus-people') 
+        else if(id == 'btn-minus-people' && tipPercent >= 0) 
         {
             document.getElementById('people').value = people - 1;
             people = parseInt(document.getElementById('people').value);
-            if (people <= 0) 
-                document.getElementById('btn-minus-people').disabled = true;
+            
         }
+        //Calling of below function to calculate the Tip per person and Total per person
         change(billAmount, tipPercent, people)
     }
+    else if (status == false && isException != true)
+        alert('Please enter valid number');
+    else if (isException == true)
+        alert('Something went wrong');
 }
+//Below function is responsible for calculating Tip per person,
+// Total per person and also responsible for displaying it.
 const change = (billAmount, tipPercent, people) => {
 
     if (tipPercent > 0 && people > 0) 
-        document.getElementById('tip-per-person').innerHTML = '$' + ((billAmount * (tipPercent / 100)) / people).toFixed(2);
+    {
+        document.getElementById('btn-minus-tip').disabled = false;
+    }
     else if (tipPercent == 0)
-        document.getElementById('tip-per-person').innerHTML = '$' + 0;
+    {
+        document.getElementById('tip-per-person').innerHTML = '$' + 0.00;
+        document.getElementById('btn-minus-tip').disabled = true;
+    }
+    else if (tipPercent < 0)
+    {
+        alert('Enter a valid Tip Percent')
+        document.getElementById('btn-minus-tip').disabled = true;
+    }
     if (people > 0)
+    {
         document.getElementById('amount-per-person').innerHTML = '$' + ((billAmount  / people )).toFixed(2);
+        document.getElementById('btn-minus-people').disabled = false;
+    }
     else if (people == 0)
-        document.getElementById('amount-per-person').innerHTML = '$' + 0;
+    {
+        document.getElementById('amount-per-person').innerHTML = '$' + 0.00;
+        document.getElementById('tip-per-person').innerHTML = '$' + 0.00;
+        document.getElementById('btn-minus-people').disabled = true;
+    }
+    else if (people < 0)
+    {
+        alert('Enter a valid number of People')
+        document.getElementById('btn-minus-people').disabled = true;
+    }
+    if (tipPercent > 0 && people > 0) 
+        document.getElementById('tip-per-person').innerHTML = '$' + ((billAmount * (tipPercent / 100)) / people).toFixed(2);
 }
